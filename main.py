e550d3a3327ed95c2a42fc2271d9e4c3c553be62
Florent T <94051212+flotql@users.noreply.github.com>
2022-01-30 00:51:46 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+game = True
+
+
+while game:
+    p1Score = 0
+    p2Score = 0
+    player1 = input("First player name:\n")
+    player2 = input("Second player name:\n")
+    Print(f"Hello {player1} and {player2}, let's the game begin hope you have fun !\n")
+    start = True
+    while start:
+        
