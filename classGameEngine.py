efe1733095451f94bfe886c977ed266a4212225d
Florent T <94051212+flotql@users.noreply.github.com>
2022-01-30 20:30:11 +0100

    Update classGameEngine.py

--- classGameEngine.py
+++ classGameEngine.py
@@ -1,7 +1,22 @@
 from classPlayer import *
-
+import random
 
 class GameEngine:
 
     def __init__(self, firstPlayer, secondPlayer):
-        self.players = Player()
+        self.firstPlayer = firstPlayer
+        self.secondPlayer = secondPlayer
+        self.p1Score = 0
+        self.p2Score = 0
+        self.players = Player(firstPlayer, secondPlayer)
+
+    def whoStart(self, firstPlayer, secondPlayer):
+        roll = random.randint(1,2)
+        if roll == 1:
+            print(f"{firstPlayer} start")
+        else:
+            print(f"{secondPlayer} start")
+
+    def pickCase(self):
+
+    def Winner(self, firstPlayer, secondPlayer):
